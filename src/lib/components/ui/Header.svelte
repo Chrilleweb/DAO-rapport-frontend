85e6c454b59159023a3333bbacb4c94dad74be7f
Chrilleweb <christian.munknissen@gmail.com>
2024-11-09 11:42:03 +0100

    added admin route til header, only admin can see that

--- src/lib/components/ui/Header.svelte
+++ src/lib/components/ui/Header.svelte
@@ -1,16 +1,20 @@
 <script>
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
-	import { faTruck, faNewspaper, faSignOutAlt, faBox, faFlag, faUserTie } from '@fortawesome/free-solid-svg-icons';
+	import { faTruck, faNewspaper, faSignOutAlt, faBox, faFlag, faUserTie, faCogs } from '@fortawesome/free-solid-svg-icons';
 	import { logoutUser } from '$lib/api/auth';
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	$: user = $page.data.user;
 
 	let menuItems = [
 		{ name: 'Samlet rapport', link: '/', icon: faFlag },
 		{ name: 'UBD', link: '/recent-ubd-reports', icon: faNewspaper },
 		{ name: 'Pakkeshop', link: '/recent-pakkeshop-reports', icon: faBox },
 		{ name: 'Indhentning', link: '/recent-indhentning-reports', icon: faTruck },
-		{ name: 'Ledelse', link: '/recent-ledelse-reports', icon: faUserTie }
-
+		{ name: 'Ledelse', link: '/recent-ledelse-reports', icon: faUserTie },
+		// Admin page 
+		{ name: 'Admin', link: '/admin', icon: faCogs, roles: ['admin'] }
 	];
 
 	async function handleLogout() {
@@ -28,15 +32,17 @@
 		<h1 class="text-5xl font-bold text-white text-center">DAO</h1>
 		<nav class="flex flex-col space-y-2 flex-grow">
 			{#each menuItems as item}
-				<a
-					href={item.link}
-					class="text-white p-2 rounded hover:bg-toggleBg flex items-center space-x-2"
-				>
-					<FontAwesomeIcon icon={item.icon} class="h-5 w-5 text-white" />
-					<span>{item.name}</span>
-				</a>
+				{#if !item.roles || item.roles.includes(user?.role)}
+					<a
+						href={item.link}
+						class="text-white p-2 rounded hover:bg-toggleBg flex items-center space-x-2"
+					>
+						<FontAwesomeIcon icon={item.icon} class="h-5 w-5 text-white" />
+						<span>{item.name}</span>
+					</a>
+				{/if}
 			{/each}
-		</nav>
+		</nav>		
 
 		<button
 			on:click={handleLogout}
