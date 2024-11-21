9f3b499710285ad4ec328f78d28f85bd33f438f0
Chrilleweb <christian.munknissen@gmail.com>
2024-11-21 21:56:12 +0100

    transports deployment socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
-    withCredentials: true // For at sikre cookies sendes
+    withCredentials: true, // For at sikre cookies sendes
+    transports: ['websocket', 'polling'] 
 });
 
 export default socket;
