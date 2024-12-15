a83f0f03da1159afd854336c6823873085e859e5
Chrilleweb <christian.munknissen@gmail.com>
2024-12-15 14:04:47 +0100

    tilføjet sikkerhedsheadere til frontend

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,14 @@
 import { validateSession } from '$lib/utils/authentication';
 
 export async function handle({ event, resolve }) {
-  validateSession(event);
-  return resolve(event);
+	validateSession(event);
+
+	const response = await resolve(event);
+
+	// Tilføj X-Frame-options header
+	response.headers.set('X-Frame-options', 'DENY');
+	// Tilføj X-Content-Type-Options header
+	response.headers.set('X-Content-Type-Options', 'nosniff');
+
+	return response;
 }
